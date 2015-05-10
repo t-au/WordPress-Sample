2dfaca286a8d60c73fd0deab1eda54ff51e5c844
t-au <t.au@live.com>
2015-05-10 06:56:07 -0700

    Header
    PHP Include

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> 
+<head>
+<html>
+<title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?></title>
+ <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/styles.css">
+  <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800' rel='stylesheet' type='text/css'>
+<!-- End Toggle Menu -->
+<!-- Start WP head -->
+<?php wp_head();?>
+<!-- End WP head -->
+</head>
+<body <?php body_class();?>>
+<div id="navbar">
+ <a href="http://tina-au.com/wordpress/">
+  <img src="<?php bloginfo('template_directory'); ?>/images/navbar.png" style="width:50px;"></a>
