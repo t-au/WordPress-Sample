da3358409657aceea4f4376a1bd928e96993653a
t-au <t.au@live.com>
2015-05-10 06:55:29 -0700

    front-page
    PHP Include

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php get_header();?>
+</div>
+
+
+
+<div id="contents">
+<?php if ( have_posts() ): while( have_posts() ) : the_post();//start the loop?>
+<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+<?php the_content();?>
+
+<?php endwhile; endif //end the loop?>
+<small>front-page.php</small>
+</ul>
+<?php get_sidebar();?>
+<?php get_footer();?>
+
