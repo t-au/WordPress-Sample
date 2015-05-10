e3026d08fb8bbaadb50644b6cee95e15829f7d73
t-au <t.au@live.com>
2015-05-10 06:54:58 -0700

    404 Error Page
    404 Error Page PHP

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header();?>
+</div>
+
+
+
+<div id="contents">
+<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+<p>WOW YOU REACH A DEAD END</p>
+<?php get_search_form();?>
+
+<small>404.php</small>
+</ul>
+<?php get_sidebar();?>
+<?php get_footer();?>
+
