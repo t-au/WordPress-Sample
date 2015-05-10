e0d6a4a9e03444403b06455c516243bdf9e26b78
t-au <t.au@live.com>
2015-05-10 06:56:00 -0700

    Functions
    PHP Include

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/*
+Theme Name: Aurem Theme
+Theme URI: http://wordpress.org/themes/auremtheme
+Author: the WordPress team
+Author URI: http://wordpress.org/
+Description: Aurem Themes
+Version: 1.0
+License: GNU General Public License v2 or later
+License URI: tina-au.com/wordpress
+Tags: black, Grey, Blue, white, flexslider
+Text Domain: Aurem Theme
+This theme, like WordPress, is licensed under the GPL.
+Use it to make something cool, have fun, and share what you've learned with others.
+*/
+
+//Register My Menus
+ register_nav_menus( array(
+  'main-menu' => __('Main'),
+  ));
+//
+
+?>
