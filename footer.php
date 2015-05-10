d39c9b535556621734471a7b1a8e142b484ba1f5
t-au <t.au@live.com>
2015-05-10 06:55:17 -0700

    Footer
    PHP Include

---
+++ footer.php
@@ -0,0 +1,10 @@
+</div>
+<ul>
+
+<div id="footer">
+<p>&copy;2015 <a href="http://www.tina-au.com">Tina Au</a> All Rights Reserved</p>
+</div>
+<!-- Star WP Footer-->
+  <?php wp_footer();?>
+  <!-- End WP Footer -->
+</html>
