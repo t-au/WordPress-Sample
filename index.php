86511ff0d911039f3893dcecbf34d69c1a94578a
t-au <t.au@live.com>
2015-05-20 21:06:39 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,16 @@
 
 <div id="contents">
 <?php if ( have_posts() ): while( have_posts() ) : the_post();//start the loop?>
+<article class="post-excerpt">
+<a href="<?php the_permalink();?>" title="Permanent Link to <?php the_title(); ?>"><?php echo get_the_post_thumbnail($post->ID, 'thumbnail');?></a>
 <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-<?php the_content();?>
-<small>index.php</small>
+<small>Posted on the <?php the_time('F  j, Y');?> in <?php the_category(', ');?></small>
+<p><?php  echo strip_tags(get_the_excerpt() );?> <a href="<?php the_permalink();?>" title="Permanent Link to <?php the_title(); ?>"">Read More &raquo;</a></p>
+</article>
 
 <?php endwhile; endif //end the loop?>
 
-
+<small>index.php</small>
 </ul>
 <?php get_sidebar();?>
 <?php get_footer();?>
