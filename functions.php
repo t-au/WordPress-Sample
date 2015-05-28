a81935b18acf68afac744b079b029ee4cb40c930
t-au <t.au@live.com>
2015-05-27 18:00:45 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -16,6 +16,15 @@ This theme, like WordPress, is licensed under the GPL.
 Use it to make something cool, have fun, and share what you've learned with others.
 */
 
+//Register My Sidebar
+register_sidebar(array(
+	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	'after_widget'  => '</div>',
+	'before_title'  => '<h2>',
+	'after_title'   => '</h2>' ,
+)); ?>
+
+
 //Register My Menus
  register_nav_menus( array(
   'main-menu' => __('Main'),
