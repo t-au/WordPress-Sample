a0e3fa2b24f9ead902f64a12217ceb24347b1321
t-au <t.au@live.com>
2015-05-27 17:56:34 -0700

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -2,6 +2,10 @@
 
 <div id="sidebar">
  
+
+
+    
+    
     <?php if (is_page()) : ?>
     <h2 class="sub-navigation-title">
     <?php echo get_the_title($post->post_parent);?></h2>
@@ -18,4 +22,11 @@ wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => __('')));
     <?php if  (!(is_page() )): ?>
     <h2>Blog</h2>
     <ul><?php wp_list_categories(array('title_li' => __('')));  ?></ul>
-    <?php endif; ?>
+         <?php endif; ?>
+
+    <?php if(get_post_meta($post->ID , 'Quote' , true)): ?>
+    
+     <blockquote><?php echo get_post_meta($post->ID , 'Quote' , true);?> <blockquote>
+         <?php endif; ?>
+    
+    
